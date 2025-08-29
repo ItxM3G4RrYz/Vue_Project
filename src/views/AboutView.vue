9fe6034d4ce7b56e8b620715b9f08b8ba9161d52
unknown <jayjaysupanat2003@gmail.com>
2025-08-29 09:20:28 +0700

    Change Com

---
+++ src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>Supanat Ponkong 67713669</h2>
+    <h3>Leave all out the rest</h3>
+    <h4>Information Technology</h4>
+  </div>
+</template>
