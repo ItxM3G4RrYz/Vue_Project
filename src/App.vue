e65ed921e11a24502341c9893ba1ccee1a7b7f01
ItxM3G4RrYz <jayjaysupanat2003@gmail.com>
2025-09-05 11:32:48 +0700

    vue not fix

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,9 @@
           </ul>
         </li>
         <li class="nav-item">
+          <a class="nav-link" href="/customers">Customer</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="/about">About</a>
         </li>
       </ul>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+    {
+    path: '/customers',
+    name: 'customers',
+    component: () => import('../views/Customers.vue')
+  },
   {
     path: '/about',
     name: 'about',
